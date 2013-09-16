9e6cbe71d71c09fec0abc09eb47305f63096ad3a
Weirch Sodora <sodora@gmail.com>
2013-09-16 23:24:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,58 +2,79 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="description" content="Short description of your site here." />
-<meta name="keywords" content="keywords, go, here, seperated, by, commas" />
-<title>Title</title>
+<meta name="description" content="Description" />
+<meta name="keywords" content="keywords1, keywords2" />
+<title>The TeloPIdb</title>
 <link href="css/style.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 </head>
 <body>
 <div id="page_wrapper">
     <div id="header_wrapper">
         <div id="header">
-        <h1 align="center">A Big Title</font></h1>
+            <p>The TeloPIdb: Mammalian telomeric proteins Interaction Database</p>
         </div>
-        <div id="navcontainer">
-        <ul id="navlist">
-            <li id="active"><a href="index.php" id="current">Home</a></li>
-            <li><a href="browser.cgi">Browser</a></li>
-            <li><a href="table.php">Table</a></li>
-        </ul>
+        <div id="nav">
+            <ul>
+                <li><a href="index.html">Home</a>
+                </li>
+                <li><a href="#">Protein-Protein</a>
+                    <ul style="display: none;">
+                        <li><a href="#">sonmenu2</a>
+                        <li><a href="#">sonmenu2</a>
+                    </ul>
+                </li>
+                <li><a href="#">Protein-DNA</a>
+                    <ul style="display: none;">
+                        <li><a href="#">sonmenu5</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Protein-RNA</a>
+                </li>
+                <li><a href="about.html">About</a>
+                </li>
+            </ul>
         </div>
+<script type="text/javascript">
+$("#nav ul li").find("a").mouseenter(function(e){
+    e.stopPropagation();
+    $(this).next().slideDown('fast');
+});
+$("#nav ul li").mouseleave(function(){
+    $(this).find("ul").slideUp('fast');
+});
+</script>
     </div>
     <div id="left_side">
       <h3 align="center">News</h3>
-      <div class='featurebox_side'>
+      <div class='box_side'>
         <p>1. 2012/12. VirMir1.0 is now released ! </p>
       </div>
 
       <h3 align="center">Links</h3>
-      <div class='featurebox_side'>
-        <p align="center"><a href="http://www.mirbase.org">miRbase</a></p>
-        <p align="center"><a href="http://www.mdc-berlin.de/en/research/research_teams/systems_biology_of_gene_regulatory_elements/projects/miRDeep/">miRDeep</a></p>
-        <p align="center"><a href="http://159.226.126.177/mirpara/">miRpara</a></p>
-        <p align="center"><a href="http://www.ncbi.nlm.nih.gov">NCBI</a></p>
-        <p align="center"><a href="http://bowtie-bio.sourceforge.net">Bowtie</a></p>
-        <p align="center"><a href="http://rfam.sanger.ac.uk/">Rfam</a></p>
-        <p align="center"><a href="http://bowtie-bio.sourceforge.net">DeepBase</a></p>
-        <p align="center"><a href="http://dsap.cgu.edu.tw/">DSAP</a></p>
+      <div class='box_side'>
+        <p><a href="http://www.mirbase.org">miRbase</a></p>
       </div>
     </div>
 
     <div id="content">
         <h3>What is VirMir? </h3>
-        <div class='featurebox_center'>
-            <span>Introduction of VirMir</span>
+        <div class='box_center'>
+            <p>Introduction of VirMir</p>
         </div>
         <h3>What can I get from VirMir? </h3>
-        <div class='featurebox_center'>
+        <div class='box_center'>
+            <p>introduction about this platform </p>
+        </div>
+        <h3>What can I get from VirMir? </h3>
+        <div class='box_center'>
             <p>introduction about this platform </p>
         </div>
     </div>
 
     <div id="footer">
-        <a href="index.html">Homepage</a> |
-        <a href="contact.html">Contact us</a>
+        <a href="index.html">Homepage</a> | 
+        <a href="contact.html">Contact us</a> 
     </div>
 </div>
 </body>
