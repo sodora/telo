1fa698b18b881a596226759d62ddb11db8bdb400
Weirch Sodora <sodora@gmail.com>
2013-09-16 08:06:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Short description of your site here." />
+<meta name="keywords" content="keywords, go, here, seperated, by, commas" />
+<title>Virus miRNA platform</title>
+<link href="css/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<div id="page_wrapper">
+    <div id="header_wrapper">
+        <div id="header">
+        <h1 align="center">A Platform for Virus microRNA Prediction </font></h1>
+        </div>
+        <div id="navcontainer">
+        <ul id="navlist">
+            <li id="active"><a href="index.php" id="current">Home</a></li>
+            <li><a href="browser.cgi">Browser</a></li>
+            <li><a href="table.php">Table</a></li>
+        </ul>
+        </div>
+    </div>
+    <div id="left_side">
+      <h3 align="center">News</h3>
+      <div class='featurebox_side'>
+        <p>1. 2012/12. VirMir1.0 is now released ! </p>
+      </div>
+
+      <h3 align="center">Links</h3>
+      <div class='featurebox_side'>
+        <p align="center"><a href="http://www.mirbase.org">miRbase</a></p>
+        <p align="center"><a href="http://www.mdc-berlin.de/en/research/research_teams/systems_biology_of_gene_regulatory_elements/projects/miRDeep/">miRDeep</a></p>
+        <p align="center"><a href="http://159.226.126.177/mirpara/">miRpara</a></p>
+        <p align="center"><a href="http://www.ncbi.nlm.nih.gov">NCBI</a></p>
+        <p align="center"><a href="http://bowtie-bio.sourceforge.net">Bowtie</a></p>
+        <p align="center"><a href="http://rfam.sanger.ac.uk/">Rfam</a></p>
+        <p align="center"><a href="http://bowtie-bio.sourceforge.net">DeepBase</a></p>
+        <p align="center"><a href="http://dsap.cgu.edu.tw/">DSAP</a></p>
+      </div>
+    </div>
+
+    <div id="content">
+        <h3>What is VirMir? </h3>
+        <div class='featurebox_center'>
+            <span>Introduction of VirMir</span>
+        </div>
+        <h3>What can I get from VirMir? </h3>
+        <div class='featurebox_center'>
+            <p>introduction about this platform </p>
+        </div>
+    </div>
+
+    <div id="footer">
+        <a href="index.html">Homepage</a> |
+        <a href="contact.html">Contact us</a>
+    </div>
+</div>
+</body>
+</html>
