0360829549d12e8dc194c60bd75a1f5a03923089
root <root@songyanglab.(none)>
2013-09-22 19:13:29 +0800

    Added new files

--- index.php
+++ index.php
@@ -1,81 +1,38 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="description" content="Description" />
-<meta name="keywords" content="keywords1, keywords2" />
-<title>The TeloPIdb</title>
-<link href="css/style.css" type="text/css" rel="stylesheet" />
-<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-</head>
-<body>
-<div id="page_wrapper">
-    <div id="header_wrapper">
-        <div id="header">
-            <p>The TeloPIdb: Mammalian telomeric proteins Interaction Database</p>
-        </div>
-        <div id="nav">
-            <ul>
-                <li><a href="index.html">Home</a>
-                </li>
-                <li><a href="#">Protein-Protein</a>
-                    <ul style="display: none;">
-                        <li><a href="#">sonmenu2</a>
-                        <li><a href="#">sonmenu2</a>
-                    </ul>
-                </li>
-                <li><a href="#">Protein-DNA</a>
-                    <ul style="display: none;">
-                        <li><a href="#">sonmenu5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Protein-RNA</a>
-                </li>
-                <li><a href="about.html">About</a>
-                </li>
-            </ul>
-        </div>
-<script type="text/javascript">
-$("#nav ul li").find("a").mouseenter(function(e){
-    e.stopPropagation();
-    $(this).next().slideDown('fast');
-});
-$("#nav ul li").mouseleave(function(){
-    $(this).find("ul").slideUp('fast');
-});
-</script>
-    </div>
-    <div id="left_side">
-      <div class='title_side'>News</div>
-      <div class='box_side'>
-        <p>1. 2012/12. VirMir1.0 is now released ! </p>
-      </div>
+<?php
+require 'common.php';
 
-      <div class='title_side'>Links</div>
-      <div class='box_side'>
-        <p><a href="http://www.mirbase.org">miRbase</a></p>
-      </div>
-    </div>
+html_header('The TeloPIdb: Home');
+html_menu();
+html_left();
+echo <<<END
+        <div class="title_center">What is TeloPIdb?</div>
+        <div class="box_center">
+            <p>Mammalian telomeric proteins Interaction Database is a collection of telomeric proteins
+            Protein-Protein interaction (PPI) data and telomeric proteins-Dna binding interaction data.
+            Collected from the scientific literature and GEO database.
+            </p>
+        </div>
+        <div class="title_center">Background</div>
+        <div class="box_center">
+            <p>Human telomeres are bound and protected by protein complexes assembled around the six
+            core telomeric proteins RAP1, TRF1, TRF2, TIN2, TPP1, and POT1. The function of these
+            proteins on telomeres has been studied exten-sively. Recently, increasing evidence has
+            suggested possible roles for these proteins outside of telomeres. However, the non-canonical
+            (extra-telomeric) function of human telomeric proteins remains poorly understood.
+            </p>
+            <p>Protein-protein interactions (PPI) represent a pivotal aspect of protein function. Almost
+            every cellular process relies on transient or permanent physical binding of two or more
+            proteins in order to accomplish the respective task. Comprehensive databases of PPI in
+            Saccharomyces cerevisiae have proved to be invaluable resources for both bioinformatics and
+            experimental research and are used heavily in the scientific community.
+            </p>
+            <p>Although yeast is a well established model organism, not all interactions in higher
+            eukaryotes have equivalent counterparts in unicellular systems. Currently, publicly available
+            PPI databases contain comparatively few entries from mammals so we embarked on building a
+            high-quality, manually curated database of protein-protein interactions in mammals.
+            </p>
+        </div>
 
-    <div id="content">
-        <div class='title_center'>What is VirMir? </div>
-        <div class='box_center'>
-            <p>Introduction of VirMir</p>
-        </div>
-        <div class='title_center'>What can I get from VirMir? </div>
-        <div class='box_center'>
-            <p>introduction about this platform </p>
-        </div>
-        <div class='title_center'>What can I get from VirMir? </div>
-        <div class='box_center'>
-            <p>introduction about this platform </p>
-        </div>
-    </div>
-
-    <div id="footer">
-        <a href="index.html">Homepage</a> | 
-        <a href="contact.html">Contact us</a> 
-    </div>
-</div>
-</body>
-</html>
+END;
+html_footer();
+?>
